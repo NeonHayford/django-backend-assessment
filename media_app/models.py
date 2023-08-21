cfa55f2185aba26d4515d3a62256217f77d46b1c
NeonHayford <hayford.lumorvi@gmail.com>
2023-08-21 10:31:31 +0000

    commit changes

--- media_app/models.py
+++ media_app/models.py
@@ -19,11 +19,25 @@ class UserGroup(models.Model):
 
 class Chat_message(models.Model):
     community = models.ForeignKey(UserGroup, on_delete=models.CASCADE, related_name='chat_group')
-    topic = models.CharField(max_length=80) #topic mist contains text, image, document, audio, etc
+    message = models.TextField(default='message') #topic mist contains text, image, document, audio, etc
     like_post = models.ManyToManyField(User, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'author_of_chat')
     created_date = models.DateTimeField(auto_now= date.today)
     modified_date = models.DateTimeField(auto_now = datetime.now())
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    chat = models.ForeignKey(Chat_message, on_delete= models.CASCADE)
+    message = models.TextField(default='comment_to_chat')
+    like_post = models.ManyToManyField(User, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+class Reply(Comment):
+    chat = models.ForeignKey(Chat_message, on_delete= models.CASCADE)
+    message = models.TextField(default='comment_to_chat')
+    like_post = models.ManyToManyField(User, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
 
 
 class follower(models.Model):
